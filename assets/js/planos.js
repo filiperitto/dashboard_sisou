1171cbd2937c56aaee8d1a70ac33591714de8f1d
Filipe Ritto <filipe.ritto@gmail.com>
2021-08-16 10:13:44 -0300

    limpeza dos arquivos do Dashboard

--- assets/js/planos.js
+++
@@ -1,9 +0,0 @@
-
-//== Lightbox About-2
-// https://biati-digital.github.io/glightbox/
-(function(){
-    var lightboxInlineIframe = GLightbox({
-        'selector': '.glightbox4'
-    });
-})();
-//======================== /Lightbox About-2
